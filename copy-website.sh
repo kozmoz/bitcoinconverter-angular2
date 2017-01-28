d02d9b26be6bbe4d4bb4072d6054a613b9593e05
Rob Juurlink <rob.juurlink@rabobank.nl>
2017-01-28 22:05:41 +0100

    update copyscript

--- copy-website.sh
+++ copy-website.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ng build --prod
-scp -r dist  vps6.juurlink.org:/home/www/sandbox.juurlink.org/http/angularjs2
+scp -r dist/*  vps6.juurlink.org:/home/www/sandbox.juurlink.org/http/angularjs2/
 
